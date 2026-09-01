5c9d61896ec984101f8bd0056117519d00acd6e7
Brenosilvxz <josephbreno72@gmail.com>
2026-09-01 14:59:51 -0300

    feat: adicionando funções ao cadastro

--- open-pizzaria/src/pages/Cadastro/index.jsx
+++ open-pizzaria/src/pages/Cadastro/index.jsx
@@ -1,47 +1,69 @@
 import React, { useState } from 'react'
+import "./Cadastro.css"
 export default function index() {
 
-    const [formdata, setData] = useState({
+    const [formData, setFormData] = useState({
     nome: "",
     email: "",
     telefone: ""
   });
 
+  //função para atualizar o estado ao digitar no formulario 
+
+  const handleChange  = (event) => {
+    const {name, value }= event.target;
+    console.log(nome,value)
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]:value,
+    }))
+
+
+  }
+
+  //funcao para enviar o formulario
+
+  const handleSubmit = (e) => {
+    e.preventdefault()
+   console.log("Enviar .....")
+  }
+
 
   return (
-   <main className='containerCadastro'>
-    <h1>Cadastro de usuarios</h1>
-    <form >
-        <article className='form-control'>
-            <label htmlFor="nome">Nome</label>
-            <input 
-            type="text"
-             name="nome"/>
+     <main className="containerCadastro">
+        <h1>Cadastro de usuários</h1>
+        <form onSubmit = {handleSubmit}>
+            <article className="form-control">
+                <label htmlFor="nome">Nome</label>
+                <input 
+                    type="text" 
+                    name="nome" 
+                    placeholder="Digite seu nome"
+                    value={formData.nome}
+                     onChange= {handleChange}
              
+                />
+            </article>
 
-          
-        </article>
+        <article className="form-control">
+                <label htmlFor="email">Email</label>
+                <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="Digite seu Email"
+                    value={formData.email}
+                />
+            </article>
 
-        <article className='form-control'>
-            <label htmlFor="gmail">gmail</label>
-            <input 
-            type="gmail" 
-            name="gmail" />
-
-
-        </article>
-
-        <article className='form-control'>
-            <label htmlFor="telefone">telefone</label>
-            <input
-             type="telefone"
-              name="telefone" />
-
-
-        </article>
-
-
-        
+       <article className="form-control">
+                <label htmlFor="telefone">Telefone</label>
+                <input 
+                    type="tel" 
+                    name="telefone" 
+                    placeholder="Digite seu telefone"
+                    value={formData.telefone}
+                />
+            </article>
 
          <br />
             <button>Cadastrar</button>
