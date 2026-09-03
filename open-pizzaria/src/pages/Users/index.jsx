290b26c8de57126ad15a7c86e699d10b40932031
Brenosilvxz <josephbreno72@gmail.com>
2026-09-03 16:02:20 -0300

    resolvendo erros do id no usuarios

--- open-pizzaria/src/pages/Users/index.jsx
+++ open-pizzaria/src/pages/Users/index.jsx
@@ -1,33 +1,61 @@
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import './Users.css'
+import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import "./Users.css"
 
-export default function Users() {
-  const [usuarios, setUsuarios] = useState([])
+export default function index() {
 
-  useEffect(() => {
-    fetch("http://localhost:3000/users")
-      .then(response => response.json())
-      .then(data => setUsuarios(data))
-      .catch(error => console.error("Erro na API", error))
-  }, [])
+    // UseState do React para gerenciar o estado dos usuários
+    const [usuarios, setUsuarios] = useState([])
 
-  console.log(usuarios)
+    // Busca os usuários quando o componente for montado
+    useEffect(() => {
 
-  return (
-    <main className="containerUsers">
-      <h1>Lista de Usuários</h1>
-      <Link to="/">Voltar para Home</Link>
+        fetch("http://localhost:3000/users")
+            .then((response) => response.json())
+            .then((data) => setUsuarios(data))
+            .catch((error) => console.error("Erro na API", error))
 
-      <section className="contentUsers">
-        {usuarios.map(user => (
-          <article key={user.id}>
-            <strong>Nome: </strong> <span>{user.nome}</span>
-            <strong>Email: </strong> <span>{user.email}</span>
-            <strong> Telefone: </strong> <span>{user.telefone}</span>
-          </article>
-        ))}
-      </section>
-    </main>
-  )
+    }, [])
+
+    console.log(usuarios)
+
+const deletarUsuario = (id) => {
+    console.log("ID clicado:", id);
+    console.log("Usuários:", usuarios);
+
+    fetch(`http://localhost:3000/users/${id}`, {
+        method: "DELETE",
+    })
+    .then(() => {
+        setUsuarios(usuarios.filter((usuario) => usuario.id !== id));
+    })
+    .catch((error) => console.error("Erro na API", error));
+};
+
+    return (
+        <main className="containerUsers">
+
+            <h1>Lista de Usuários</h1>
+
+            <section className="contentUsers">
+            {usuarios.map((user) => (
+                <article key={user.id}>
+                    <strong>Nome: {user.nome}</strong>
+                    <strong>Email: {user.email}</strong>
+                    <strong>Telefone: {user.telefone}</strong>
+                    <button 
+                        className="btn-delete"
+                         onClick={() => deletarUsuario(user.id)}>
+                        Deletar {user.nome}
+                    </button>
+
+                </article>
+            ))}
+
+            </section>
+
+            <Link to="/">Voltar para HOME</Link>
+
+        </main>
+    )
 }
